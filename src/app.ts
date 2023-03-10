b2ba9bb78d946cbdf9fab3eec5f4083e24b0271a
Dc-Valerian <valerianpedro03@gmail.com>
2023-03-10 13:14:03 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
-import express,{Application} from "express"
+import express,{Application,Request,Response,NextFunction} from "express"
 import cors from "cors"
 import morgan from "morgan"
+import { errorHandler } from "./middleware/errorHandler"
 
 
 const appConfig = (app:Application)=>{
     app.use(express.json()).use(cors()).use(morgan("dev"))
 
+    
+
+    // CHECKING FOR UNAVAILBLE ROUTES
+    .all("*",(req:Request,res:Response,next:NextFunction)=>{
+        res.status(404).json({
+            message:`This Route ${req.originalUrl} doesn't exist`
+        })
+    })
     // Error Handler
-    .use()
+    .use(errorHandler)
 }
 
 export default appConfig
