2727bc2cd7e71697b4282480101a65728e7af5f6
Dc-Valerian <valerianpedro03@gmail.com>
2023-03-10 12:54:13 +0100

    updated the errorHandler

--- src/middleware/errorHandler/index.ts
+++ src/middleware/errorHandler/index.ts
@@ -2,10 +2,15 @@ import {Request,Response,NextFunction} from "express"
 import { AppError,HtppCode } from "../../utils/AppError"
 
 const devErrors =(err:AppError,res:Response)=>{
-    return res.status
+    return res.status(HtppCode.INTERNAL_SEVER_ERRO).json({
+        error:err,
+        message:err.message,
+        stack:err.stack,
+        status:err.httpCode
+    })
 }
 
 
 export const errorHandler =(err:AppError,req:Request,res:Response,next:NextFunction)=>{
-
+    devErrors(err,res)
 }
